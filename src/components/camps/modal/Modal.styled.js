f8af13ce0efc12e8de3a91c720f47a653abbce64
Julie Neuburger <juliehockey30@gmail.com>
2019-12-23 08:24:19 -0700

    alphabetize styles in modal

--- src/components/camps/modal/Modal.styled.js
+++ src/components/camps/modal/Modal.styled.js
@@ -1,82 +1,81 @@
 import styled from 'styled-components'
 
 const ModalOverLay = styled.div`
-  position: fixed;
-  z-index: 1;
-  padding-top: 100px;
+  background-color: rgba(0,0,0,0.4);
+  height: 100%;
   left: 0;
+  padding-top: 100px;
+  position: fixed;
   top: -40px;
   width: 100%;
-  height: 100%;
-  background-color: rgba(0,0,0,0.4);
+  z-index: 1;
 `;
 
 const ModalContent = styled.div`
   background-color: white;
   border: 3px solid black;
-  position: absolute;
   left: 1%;
-  width: 90%;
-  padding: 12px;
   max-height: calc(100vh - 100px);
   overflow-y: auto;
+  padding: 12px;
+  position: absolute;
+  width: 90%;
 
   @media(min-width: 768px) {
-    width: 70%;
     left: 15%;
-    top: 150px;
-    padding: 24px;
     max-height: calc(100vh - 210px);
+    padding: 24px;
+    top: 150px;
+    width: 70%;
   }
 `;
 
 const CloseIcon = styled.div`
-  text-align: right;
-  margin: 0 12px 0 0;
+  background-color: white;
+  cursor: pointer;
   font-size: 24px;
   font-weight: bold;
-  cursor: pointer;
-  position: fixed;
-  right: 135px;
-  width: 73%;
-  background-color: white;
-  top: 113px;
   height: 40px;
   line-height: 40px;
+  margin: 0 12px 0 0;
+  position: fixed;
+  right: 135px;
+  text-align: right;
+  top: 113px;
+  width: 73%;
 `;
 
 const ModalTitle = styled.div`
-  text-align: center;
-  margin-top: 0;
   color: rgb(103, 187, 240);
   font-size: 48px;
   font-weight: bold;
   margin-top: 20px;
+  text-align: center;
 `;
 
 const ModalDescription = styled.div`
-  text-align: center;
-  font-weight: bold;
   font-size: 16px;
-  margin-top: 16px;
+  font-weight: bold;
   margin-bottom: 12px;
+  margin-top: 16px;
+  text-align: center;
 `;
 
 const ModalLabel = styled.span`
-  text-align: center;
-  font-weight: bold;
   font-size: 20px;
-  margin-top: 12px;
+  font-weight: bold;
   margin-bottom: 12px;
+  margin-top: 12px;
+  text-align: center;
 `;
 
 const ModalValue = styled.span`
   color: rgb(103, 187, 240);
-  text-align: center;
-  font-weight: bold;
   font-size: 20px;
-  margin-top: 12px;
+  font-weight: bold;
   margin-bottom: 12px;
+  margin-top: 12px;
+  text-align: center;
 `;
 
 const CostExplination = styled.div`
@@ -84,8 +83,8 @@ const CostExplination = styled.div`
 `;
 
 const MetaDataWrapper = styled.div`
-  margin-top: 16px;
   margin-bottom: 16px;
+  margin-top: 16px;
 `;
 
 const ButtonWrapper = styled.div`
