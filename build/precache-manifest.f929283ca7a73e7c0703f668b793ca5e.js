b72621e0ee5024bc99933f06d61713d2aa560590
Julie Neuburger <juliehockey30@gmail.com>
2020-03-05 15:52:56 -0700

    don't ignore build folder

---
+++ build/precache-manifest.f929283ca7a73e7c0703f668b793ca5e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bfb29270603bf6573343e7248235ec7",
+    "url": "/rickardHockey/index.html"
+  },
+  {
+    "revision": "9b2c0a58aca2ebdd4567",
+    "url": "/rickardHockey/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f1b74ccc2ce77be7d811",
+    "url": "/rickardHockey/static/js/2.620c8ab1.chunk.js"
+  },
+  {
+    "revision": "9b2c0a58aca2ebdd4567",
+    "url": "/rickardHockey/static/js/main.bda3e76a.chunk.js"
+  },
+  {
+    "revision": "5ec7314ca5507842b228",
+    "url": "/rickardHockey/static/js/runtime-main.2e82a0cd.js"
+  },
+  {
+    "revision": "77e8cf6183150e905b43ac7935314239",
+    "url": "/rickardHockey/static/media/IMG_5338.77e8cf61.JPG"
+  },
+  {
+    "revision": "1adc85d524204971a3a4cb51cc866612",
+    "url": "/rickardHockey/static/media/IMG_5553.1adc85d5.JPG"
+  },
+  {
+    "revision": "82bf07b9c73953f4cede3a3319822d98",
+    "url": "/rickardHockey/static/media/logo.82bf07b9.png"
+  },
+  {
+    "revision": "6156ca3d099e6a92f498ebd044746f38",
+    "url": "/rickardHockey/static/media/venmo_logo_blue.6156ca3d.svg"
+  },
+  {
+    "revision": "b3f607fcfa5191e0503766ce907754e0",
+    "url": "/rickardHockey/static/media/zele-logo.b3f607fc.png"
+  }
+]);
